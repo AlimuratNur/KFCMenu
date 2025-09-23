981b786ee8d99b87347d12ac1da5abe33e6d2b0c
alimratnurgajn@gmail.com <alimratnurgajn@gmail.com>
2025-09-23 20:24:44 +0500

    Was added ViewModelBase

---
+++ ViewModes/base/ViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace KFCMenu.ViewModes.Base
+{
+    internal class ViewModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+
+        public void Dispose()
+        {
+
+        }
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!_Disposed || _Disposed) return;
+            _Disposed = true;
+        }
+    }
+}
