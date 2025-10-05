dc71e9ffe1741022e9b39738ae21769684c5e254
AlimuratNur <alimratnurgajn@gmail.com>
2025-10-05 20:33:32 +0500

    added rowDefinitions

--- ViewModes/MainViewModel.cs
+++ ViewModes/MainViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KFCMenu.ViewModes
+namespace KFCMenu.ViewModel
 {
     internal class MainViewModel
     {
--- ViewModes/base/ViewModel.cs
+++ ViewModes/base/ViewModel.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace KFCMenu.ViewModes.Base
+namespace KFCMenu.ViewModel.Base
 {
     internal class ViewModel
     {
