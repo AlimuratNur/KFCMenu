bb7e3c3c40b3a74602124ef43bd4c385cde162a8
AlimuratNur <alimratnurgajn@gmail.com>
2025-10-08 17:22:47 +0500

    was added file "jsonDataService" with class that has identifical name, that loads json

---
+++ Services/JsonDataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFCMenu.Services
+{
+    public class JsonDataService
+    {
+        private readonly string _FilePath;
+        
+        public JsonDataService(string filePath)
+        {
+            _FilePath = filePath;
+        }
+
+        public Task<string> LoadAsync(string path) 
+        {
+
+        }
+
+    }
+}
