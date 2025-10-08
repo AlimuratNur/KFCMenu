6db62ac78565abf25edc80625ec11229ab598e66
AlimuratNur <alimratnurgajn@gmail.com>
2025-10-08 20:54:27 +0500

    Revert "added "Data" folder, where we will contain json files"
    This reverts commit 87674528d833fd635ebdfe3ec3f7109f84e36077.

--- Tests/JsonServicesDataTests.cs
+++ Tests/JsonServicesDataTests.cs
@@ -6,20 +6,13 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
 using KFCMenu.Services;
-using KFCMenu.Models;
 
 namespace KFCMenu.Tests;
 [TestFixture]
 public class JsonServicesDataTests
 {
     [Test]
-    public void ReturnEmptyList()
-    {
-        var jsonTest = new JsonDataService();
-
-    }
-
-    public void MainTest(List<Dish> expectedDishes, List<Dish> realDishes)
+    public void MainTest()
     {
         var filePath = "";
     }
