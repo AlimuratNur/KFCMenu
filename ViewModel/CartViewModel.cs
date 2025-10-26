bd3d3692b5a32ce7c360fde5b0dc78e0fd84031f
AlimuratNur <alimratnurgajn@gmail.com>
2025-10-26 22:57:27 +0500

    before total re building

--- ViewModel/CartViewModel.cs
+++ ViewModel/CartViewModel.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KFCMenu.Models;
 using KFCMenu.ViewModel.Base;
 
 namespace KFCMenu.ViewModel
 {
-    class CartViewModel : ViewModelBase
+    public class CartViewModel : ViewModelBase
     {
-
+        public ICollection<Dish> Dishes { get;  }
+        public CartViewModel()
+        {
+            Dishes = new List<Dish>();
+        }
     }
 }
