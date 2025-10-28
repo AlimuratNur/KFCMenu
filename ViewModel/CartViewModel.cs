9d2dd2347bd6dda84068245ffbdb333d30aa20b5
AlimuratNur <alimratnurgajn@gmail.com>
2025-10-28 21:21:02 +0500

    more features added \

--- ViewModel/CartViewModel.cs
+++ ViewModel/CartViewModel.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
-using KFCMenu.Models;
+using KFCMenu.Services;
 using KFCMenu.ViewModel.Base;
 
 namespace KFCMenu.ViewModel
 {
     public class CartViewModel : ViewModelBase
     {
-        public ICollection<Dish> Dishes { get;  }
+        public DishCart Dishes { get;  }
         public CartViewModel()
         {
-            Dishes = new List<Dish>();
+            Dishes = new();
+
         }
     }
 }
